c422e060d18ce79a4968b2be7e3c3b4e21f6228b
David Rivas <105255097+davidfrivas@users.noreply.github.com>
2025-04-28 17:24:54 -0400

    Add dependencies

---
+++ CPM_modules/FindJUCE.cmake
@@ -0,0 +1,3 @@
+include("/Users/davidrivas/Developer/repos/JUCECmakeRepoPrototype/CMake/CPM.cmake")
+CPMAddPackage("GITHUB_REPOSITORY;juce-framework/JUCE;GIT_TAG;develop;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
+set(JUCE_FOUND TRUE)
